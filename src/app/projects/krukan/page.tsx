587be87e0d5d7b5819792c18ae92e5c1b8f556f1
Philip Carlsson <philipcarlsson@hotmail.com>
2023-03-27 11:13:05 +0200

    adding pictures for krukan

--- src/app/projects/krukan/page.tsx
+++ src/app/projects/krukan/page.tsx
@@ -1,17 +1,30 @@
 "use client";
+import { ProjectImage } from "@/components/image/Image";
 import { TextSection } from "@/components/textSection/TextSection";
 import { krukan } from "@/utils/krukan/info";
 
+const images = [
+  "/krukan/landing-page.jpg",
+  "/krukan/match-roster.jpg",
+  "/krukan/match-schedule.jpg",
+  "/krukan/db-matches.jpg",
+];
+
 const KrukanPage = () => {
   return (
-    <TextSection
-      header={krukan.header}
-      timeSpent={krukan.timeSpent}
-      github={krukan.github}
-      website={krukan.website}
-      tags={krukan.tags}
-      text={krukan.text}
-    />
+    <>
+      <TextSection
+        header={krukan.header}
+        timeSpent={krukan.timeSpent}
+        github={krukan.github}
+        website={krukan.website}
+        tags={krukan.tags}
+        text={krukan.text}
+      />
+      {images.map((img, i) => (
+        <ProjectImage key={i} src={img} alt={"krukan"} />
+      ))}
+    </>
   );
 };
 
--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
+import styled from "./Image.module.css";
 
-export const Img = () => {
-  return <></>;
+interface Props {
+  src: string;
+  alt: string;
+}
+
+export const ProjectImage: React.FC<Props> = ({ src, alt }) => {
+  return (
+    <div className={styled.imgDiv}>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        placeholder="blur"
+        blurDataURL={src}
+        loading="lazy"
+      />
+    </div>
+  );
 };
