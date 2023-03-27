6e496c39a7269e01315415b35a10cdc2a8665379
Philip Carlsson <philipcarlsson@hotmail.com>
2023-03-27 11:41:08 +0200

    adding platzi images

--- src/app/projects/platzis-paradise/page.tsx
+++ src/app/projects/platzis-paradise/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
+import { ProjectImage } from "@/components/image/Image";
 import { TextSection } from "@/components/textSection/TextSection";
 import { webshop } from "@/utils/platzisParadise/info";
 
+const images = [
+  "/platzi/user-page.jpg",
+  "/platzi/paginated-products.jpg",
+  "/platzi/cart.jpg",
+  "/platzi/order-placed.jpg",
+  "/platzi/register.jpg",
+  "/platzi/admin.jpg",
+  "/platzi/categories.jpg",
+];
+
 const WebShopPage = () => {
   return (
-    <TextSection
-      header={webshop.header}
-      timeSpent={webshop.timeSpent}
-      github={webshop.github}
-      tags={webshop.tags}
-      text={webshop.text}
-    />
+    <>
+      <TextSection
+        header={webshop.header}
+        timeSpent={webshop.timeSpent}
+        github={webshop.github}
+        tags={webshop.tags}
+        text={webshop.text}
+      />
+      {images.map((img) => (
+        <ProjectImage key={img} src={img} alt={"Webshop examination"} />
+      ))}
+    </>
   );
 };
 
